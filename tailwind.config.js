a42f1654037ba06c84004286e226fc9f097e7114
khalilbadreddine <khalilbadre5@gmail.com>
2024-12-08 20:29:22 +0100

    Fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-/* eslint-disable no-undef */
 // tailwind.config.js
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 
-module.exports = withMT({
+export default withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path to match where your files are
   ],
   theme: {
     extend: {
-       fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
-    },},
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 });
